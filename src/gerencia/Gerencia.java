6da0ca6980617c1d3ecaa424d214cd351cf55650
Diego Oliveira Froes <Diego Oliveira Froes@ChuckNorris.mshome.net>
2013-01-26 22:58:01 -0200

    Commit inicial do projeto.

---
+++ src/gerencia/Gerencia.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerencia;
+
+/**
+ *
+ * @author Diego Oliveira Froes
+ */
+public class Gerencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Utilizando o GIT através do Netbeans.");
+    }
+}
